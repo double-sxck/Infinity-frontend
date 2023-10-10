5aaf623e63aa0b59a2d9f02f432eaa6601c55f4e
useonguk <ysu0219@gmail.com>
2023-10-10 15:22:07 +0900

    SET : folder

---
+++ src/components/alert/index.tsx
@@ -0,0 +1,7 @@
+// export { default as Header } from "";
+// export { default as MainText } from "";
+// export { default as SendIcon } from "";
+// export { default as TextField } from "";
+
+// import { Header, MainText, TextField } from "./components/index";
+// 요걸로 import 해서 사용하면 됨
