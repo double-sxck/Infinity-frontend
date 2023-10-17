26f4e4dd24dd366565f34be1cf0762a51d3f9991
useonguk <ysu0219@gmail.com>
2023-10-17 16:56:21 +0900

    CHORE

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Main } from "./components/index";
+import { Main, Write } from "./components/index";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./reset.css";
 
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/write" element={<Write />} />
         <Route path="/about" element={<div>sexy</div>} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 export { default as Header } from "./header/header";
 export { Main } from "./main/index";
+export { default as Write } from "./writePage/writePage";
