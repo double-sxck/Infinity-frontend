d3427a5073d034b320bec41da40e31002b494e8f
HUN1i <nicebrian0328@gmail.com>
2023-10-20 10:44:49 +0900

    FIX :: header, writepage 위주 수정

--- src/components/header/headerStyle.ts
+++ src/components/header/headerStyle.ts
@@ -6,9 +6,9 @@ export const headerBody = styled.div`
   width: 100vw;
   height: 4.6em;
   background-color: #000000;
-  position: fixed;
   top: 0;
   display: flex;
+  justify-content: center;
   align-items: center;
   padding-left: 1em;
 `;
@@ -17,21 +17,19 @@ export const logoBackground = styled.div`
   width: 100px;
   height: 2.5em;
   background-color: white;
-  position: fixed;
-  left: 30px;
-  top: 10px;
+  margin-right: auto;
 `;
 
 export const headerButtons = styled(Link)`
-  width: 3.5em;
+  width: 5em;
   height: 2em;
   background-color: #f5f5f5;
   border-radius: 24px;
+  margin-left: auto;
   display: flex;
   justify-content: center;
   align-items: center;
   text-decoration: none;
-  margin-left: 1em;
 `;
 
 export const loginButton = styled.div`
@@ -63,7 +61,6 @@ export const searchBox = styled.input`
   height: 3em;
   border-radius: 30px;
   padding-left: 40px; /* 아이콘과 텍스트 간의 간격을 조절하기 위해 추가 */
-  margin: auto;
   &::placeholder {
     background-image: url("../../assets/images/search.svg");
     background-size: contain;
@@ -73,8 +70,6 @@ export const searchBox = styled.input`
 `;
 
 export const postionFiexd = styled.div`
-  position: fixed;
   display: flex;
-  top: 10px;
   right: 30px;
 `;
