27a2a0b2c8ab2273c4655b3c7dac31a5513657f5
마현우 <mhw35689402@gmail.com>
2023-10-21 16:27:58 +0900

    add: 로고 클릭시 메인으로 이동하도록 변경

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,18 +2,12 @@
 
 import * as S from "./headerStyle";
 import React, { useState } from "react";
-import Login from "../login/login";
 import { ReactComponent as Logo } from "../../assets/images/logo_v2.svg";
 import { ReactComponent as SearchIcon } from "../../assets/images/search.svg";
 import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
-  const [isLogin, setIsLogin] = useState({
-    popup: false,
-    login: false,
-  });
   const [search, setSearch] = useState("");
-  const [user, setUser] = useState("");
 
   const seachHandle = (e: any) => {
     setSearch(e.target.value);
@@ -26,7 +20,9 @@ const Header: React.FC = () => {
     <>
       <S.container>
         <S.alignLeft>
-          <Logo />
+          <Link to="/">
+            <Logo />
+          </Link>
           <SearchIcon
             style={{ position: "relative", left: "35px", margin: "auto 0" }}
           />
